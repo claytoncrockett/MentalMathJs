4c861c61038421350e5923fe278941c579e4bbc5
Clayton Crockett <claytoncrockett@gmail.com>
2018-12-12 16:45:44 -0700

    score

--- src/Game.js
+++ src/Game.js
@@ -23,23 +23,42 @@ class Game extends React.Component {
       id: this.state.id + 1
     });
 
-    if (this.state.answers.length < 5) {
-      setTimeout(() => {
+    setTimeout(() => {
+      if (this.state.answers.length < 10) {
         this.makeNewItem();
-      }, 10000);
+      }
+    }, 10000);
+  };
+
+  checkForAnswer = answerCheck => {
+    const arr = this.state.answers;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === answerCheck) {
+        this.setState({ score: this.state.score + 1 });
+      }
     }
   };
 
   handleSubmit = () => {
-    console.log("hi");
+    let answerCheck = parseInt(this.state.userAnswer, 10);
+    this.checkForAnswer(answerCheck);
+    let newAnswers = this.state.answers.filter(
+      answer => answer !== answerCheck
+    );
+    let newProblems = this.state.problems.filter(
+      problem => answerCheck !== problem[0] + problem[1]
+    );
+    this.setState({
+      answers: newAnswers,
+      problems: newProblems,
+      userAnswer: ""
+    });
   };
 
   handleKey = event => {
     if (!isNaN(event.key)) {
       if (this.state.userAnswer.length > 1) {
         this.setState({ userAnswer: event.key });
-      } else if (event.key === "Enter") {
-        this.handleSubmit();
       } else {
         this.setState({ userAnswer: this.state.userAnswer + event.key });
       }
@@ -52,8 +71,9 @@ class Game extends React.Component {
           )
         });
       }
+    } else if (event.key === "Enter") {
+      this.handleSubmit();
     }
-    console.log(this.state.userAnswer);
   };
 
   componentDidMount() {
@@ -69,6 +89,7 @@ class Game extends React.Component {
     const { answers, problems } = this.state;
     return (
       <div id="game">
+        <div id="score">Score: {this.state.score}</div>
         {this.state.playing &&
           problems.map(problem => {
             return (
